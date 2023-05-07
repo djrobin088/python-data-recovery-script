688a00b81ad0f7f2efbcedb77e5a05ef1eccbb65
djrobin088 <81708365+djrobin088@users.noreply.github.com>
2023-05-07 12:09:38 +0600

    Add files via upload

---
+++ recovery_script.py
@@ -0,0 +1,30 @@
+drive = "\\\\.\\E:"     # Open drive as raw bytes
+fileD = open(drive, "rb")
+size = 512              # Size of bytes to read
+byte = fileD.read(size) # Read 'size' bytes
+offs = 0                # Offset location
+drec = False            # Recovery mode
+rcvd = 0                # Recovered file ID
+while byte:
+    found = byte.find(b'\x66\x74\x79\x70\x69\x73\x6F\x6D')
+
+    if found >= 0:
+        drec = True
+        print('==== Found JPG at location: ' + str(hex(found+(size*offs))) + ' ====')
+        # Now lets create recovered file and search for ending signature
+        fileN = open(str(rcvd) + '.jpg', "wb")
+        fileN.write(byte[found:])
+        while drec:
+            byte = fileD.read(size)
+            bfind = byte.find(b'\xff\xd9')
+            if bfind >= 0:
+                fileN.write(byte[:bfind+2])
+                fileD.seek((offs+1)*size)
+                print('==== Wrote JPG to location: ' + str(rcvd) + '.jpg ====\n')
+                drec = False
+                rcvd += 1
+                fileN.close()
+            else: fileN.write(byte)
+    byte = fileD.read(size)
+    offs += 1
+fileD.close()
